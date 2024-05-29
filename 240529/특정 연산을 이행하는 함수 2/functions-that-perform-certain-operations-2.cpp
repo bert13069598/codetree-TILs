0023ada108e3a98fc5d6c903f105141b5e35a42e
Sungbin Shim <everglow83@naver.com>
2024-05-29 22:38:14 +0900

    Title: 특정 연산을 이행하는 함수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240529/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.cpp
+++ 240529/특정 연산을 이행하는 함수 2/functions-that-perform-certain-operations-2.cpp
@@ -5,16 +5,14 @@
 
 using namespace std;
 int main() {
-    vector<pair<double,int>> num(3);
+    vector<double> num(3);
     for(int i=0;i<3;i++){
-        cin >> num[i].first;
-        num[i].second=i;
+        cin >> num[i];
     }
     sort(num.begin(),num.end());
-    num[0].first = int(num[0].first);
-    num[1].first = round(num[1].first);
-    num[2].first = int(num[2].first)+1;
-    sort(num.begin(),num.end(), [](pair<double,int> &a,pair<double,int> &b){return a.second < b.second;});
-    cout << num[0].first <<" "<< num[1].first<<" "<<num[2].first;
+    num[0]=int(num[0]);
+    num[1]=round(num[1]);
+    num[2]=int(num[2])+1;
+    cout << num[2] <<" " <<num[0]<<" "<<num[1];
     return 0;
 }
