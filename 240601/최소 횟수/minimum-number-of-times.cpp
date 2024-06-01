6b83ef64af6de237c2bd666cc1f379f9aaff048a
Sungbin Shim <everglow83@naver.com>
2024-06-01 14:17:11 +0900

    Title: 최소 횟수, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

--- 240601/최소 횟수/minimum-number-of-times.cpp
+++ 240601/최소 횟수/minimum-number-of-times.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 #define MAX 1e5
 
 using namespace std;
@@ -8,19 +9,35 @@ int main() {
     int a,b;
     cin>>a>>b;
     deque<pair<int,int>> q;
+    vector<bool> visit(MAX);
     q.emplace_back(a,0);
+    visit[a]=true;
     while(!q.empty()){
         pair now = q.front();
         if(now.first==b){
             cout << now.second;
             break;
         }
+        // cout << now.first <<"\n";
         q.pop_front();
-        q.emplace_back(now.first+1,now.second+1);
-        if(now.first-1>0)
-            q.emplace_back(now.first-1,now.second+1);
-        if(now.first*2<MAX)
-            q.emplace_back(now.first*2,now.second);
+        if(now.first<MAX && now.first<b){
+            if(!visit[now.first+1]){
+                visit[now.first+1]=true;
+                q.emplace_back(now.first+1,now.second+1);
+            }
+        }
+        if(now.first-1>=0){
+            if(!visit[now.first-1]){
+                visit[now.first-1]=true;
+                q.emplace_back(now.first-1,now.second+1);
+            }
+        }
+        if(now.first<MAX && now.first<b){
+            if(!visit[now.first*2]){
+                visit[now.first*2]=true;
+                q.emplace_back(now.first*2,now.second);
+            }
+        }
     }
     return 0;
 }
