c5fe3fa580a96f81efa7fba1fb7c10564ecfcb2c
Sungbin Shim <everglow83@naver.com>
2024-06-01 14:26:54 +0900

    Title: 최소 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240601/최소 횟수/minimum-number-of-times.cpp
+++ 240601/최소 횟수/minimum-number-of-times.cpp
@@ -9,7 +9,7 @@ int main() {
     int a,b;
     cin>>a>>b;
     deque<pair<int,int>> q;
-    vector<bool> visit(MAX);
+    vector<bool> visit(MAX*2);
     q.emplace_back(a,0);
     visit[a]=true;
     while(!q.empty()){
@@ -20,19 +20,19 @@ int main() {
         }
         // cout << now.first <<"\n";
         q.pop_front();
-        if(now.first<MAX && now.first<b){
+        if(now.first<b){
             if(!visit[now.first+1]){
                 visit[now.first+1]=true;
                 q.emplace_back(now.first+1,now.second+1);
             }
         }
-        if(now.first-1>=0){
+        if(now.first>0){
             if(!visit[now.first-1]){
                 visit[now.first-1]=true;
                 q.emplace_back(now.first-1,now.second+1);
             }
         }
-        if(now.first<MAX && now.first<b){
+        if(now.first<b){
             if(!visit[now.first*2]){
                 visit[now.first*2]=true;
                 q.emplace_back(now.first*2,now.second);
