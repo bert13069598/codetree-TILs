bc149caef6b8fb471ae4692e41e413cc50d50e74
Sungbin Shim <everglow83@naver.com>
2024-06-07 22:56:15 +0900

    Title: 오목의 승패, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240607/오목의 승패/concave-victory-and-defeat.cpp
+++ 240607/오목의 승패/concave-victory-and-defeat.cpp
@@ -13,12 +13,11 @@ void dfs2(int n, int now, int *map,int team,int i,int d){
         return;
     if(abs(dxy[i])==1&&now/n!=next/n)
         return;
-    if(map[next]==team){
+    if(map[next]==team)
         dfs2(n,next,map,team,i,d+1);
-    }
 }
 
-void dfs(int n,int now, int *map,int team){
+void dfs(int n,int now, int *map,int team,int d){
     int dxy[8]={1,-1,n,-n,n+1,n-1,-n+1,-n-1};
     for(int i=0;i<8;i++){
         int next=now+dxy[i];
@@ -26,9 +25,8 @@ void dfs(int n,int now, int *map,int team){
             continue;
         if(abs(dxy[i])==1&&now/n!=next/n)
             continue;
-        if(map[next]==team){
-            dfs2(n,next,map,team,i,2);
-        }
+        if(map[next]==team)
+            dfs2(n,next,map,team,i,d+1);
     }
 }
 
@@ -52,7 +50,7 @@ int main() {
             team=map[i];
             xy={i/n+1,i%n+1};
 
-            dfs(n,i,map,team);
+            dfs(n,i,map,team,1);
             if(win){
                 if(team==1)
                     cout << 1 << "\n";
