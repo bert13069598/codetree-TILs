e7e67e257985cf76ec348d980ed9a48e8a24a09f
Sungbin Shim <everglow83@naver.com>
2024-06-07 18:45:38 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240607/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
+++ 240607/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -10,9 +10,9 @@ int main() {
     
     dp[0][0]=map[0][0];
     for(int i=0;i<n-1;i++){
-        dp[0][i+1]=min(map[0][i],map[0][i+1]);
-        dp[i+1][0]=min(map[i][0],map[i+1][0]);
-    }
+        dp[0][i+1]=min(dp[0][i],map[0][i+1]);
+        dp[i+1][0]=min(dp[i][0],map[i+1][0]);
+    }   
     for(int i=1;i<n;i++)
         for(int j=1;j<n;j++)
             dp[i][j]=min(max(dp[i][j-1],dp[i-1][j]),map[i][j]);
