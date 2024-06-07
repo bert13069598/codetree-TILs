99ad33b1d0f86a89aec685154d1b7fbef79a742c
Sungbin Shim <everglow83@naver.com>
2024-06-07 22:25:05 +0900

    Title: 오목의 승패, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240607/오목의 승패/concave-victory-and-defeat.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int sum=1;
+
+void dfs2(int n, int now, int *map,int team,int i){
+    int dxy[8]={1,-1,n,-n,n+1,n-1,-n+1,-n-1};
+    int next=now+dxy[i];
+    if(next<0||next>=n*n)
+        return;
+    if(abs(dxy[i])==1&&now/n!=next/n)
+        return;
+    if(map[next]==team){
+        sum++;
+        map[next]=0;
+        dfs2(n,next,map,team,i);
+    }
+}
+
+void dfs(int n,int now, int *map,int team){
+    int dxy[8]={1,-1,n,-n,n+1,n-1,-n+1,-n-1};
+    for(int i=0;i<8;i++){
+        int next=now+dxy[i];
+        if(next<0||next>=n*n)
+            continue;
+        if(abs(dxy[i])==1&&now/n!=next/n)
+            continue;
+        if(map[next]==team){
+            sum++;
+            map[next]=0;
+            dfs2(n,next,map,team,i);
+        }
+    }
+}
+
+int main() {
+    int n=19;
+    int map[n*n];
+    for(int i=0;i<n*n;i++)
+        cin>>map[i];
+    int team = 0;
+    pair<int,int> xy;
+    for(int i=0;i<n*n;i++){
+        if(map[i]){
+            team=map[i];
+            xy={i/n+1,i%n+1};
+
+            map[i]=0;
+            sum=1;
+            dfs(n,i,map,team);
+            if(sum==5){
+                if(team==1)
+                    cout << 1 << "\n";
+                else if(team==2)
+                    cout << 2 << "\n";
+                cout << xy.first << " " << xy.second;
+                return 0;
+            }
+        }
+    }
+    cout << 0;
+    return 0;
+}
