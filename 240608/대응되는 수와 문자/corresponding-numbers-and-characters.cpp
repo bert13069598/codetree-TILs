d0222a84b8643e964e883933658b7b69a2d72f03
Sungbin Shim <everglow83@naver.com>
2024-06-08 18:38:51 +0900

    Title: 대응되는 수와 문자, Time: 133ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240608/대응되는 수와 문자/corresponding-numbers-and-characters.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main() {
+    int n,m;
+    cin>>n>>m;
+    unordered_map<string,int> m1;
+    unordered_map<int,string> m2;
+    string s;
+    for(int i=1;i<=n;i++){
+        cin>>s;
+        m1[s]=i;
+        m2[i]=s;
+    }
+    for(int i=0;i<m;i++){
+        cin>>s;
+        if(isdigit(s[0]))
+            cout<<m2[s[0]-'0']<<"\n";
+        else
+            cout<<m1[s]<<"\n";
+    }
+
+    return 0;
+}
