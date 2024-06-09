1fce0d4d2cd455c9b7e19cc0d57c754852fb9a40
Sungbin Shim <everglow83@naver.com>
2024-06-09 15:40:31 +0900

    Title: 오목의 승패, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240609/오목의 승패/concave-victory-and-defeat.cpp
+++ 240609/오목의 승패/concave-victory-and-defeat.cpp
@@ -2,14 +2,14 @@
 #include <vector>
 using namespace std;
 
-int dx[4]={0,1,0,1};
-int dy[4]={1,1,1,-1};
+int dx[4]={0,1,1,-1};
+int dy[4]={1,0,1,1};
 
 int dfs(int n, int x, int y, vector<vector<int>> &map, int dir){
     int nx = x + dx[dir];
     int ny = y + dy[dir];
     if(nx<0||ny<0||nx>=n||ny>=n)
-        return 0;
+        return 2;
     if(map[nx][ny]==map[x][y]){
         int k = dfs(n,nx,ny,map,dir);
         // cout <<k << "("<<x<<","<<y<<")";
@@ -22,7 +22,7 @@ int main() {
     vector<vector<int>> map(n,vector<int>(n));
     for(int i=0;i<n;i++)
         for(int j=0;j<n;j++)
-            cin>>map[j][i];
+            cin>>map[i][j];
     
     int team;
     pair<int,int> xy;
@@ -37,13 +37,17 @@ int main() {
                 for(int k=0;k<4;k++){
                     int nx = x + dx[k];
                     int ny = y + dy[k];
+                    int px = x - dx[k];
+                    int py = y - dy[k];
                     if(nx<0||ny<0||nx>=n||ny>=n)
+                        continue;
+                    if(px>=0&&py>=0&&px<n&&py<n && map[px][py]==map[x][y])
                         continue;
                     if(map[nx][ny]==map[x][y]){
                         int d = dfs(n,nx,ny,map,k);
                         if(d==5){
                             cout << team << "\n";
-                            cout << xy.second + 1 << " " << xy.first + 1;
+                            cout << xy.first + 1 << " " << xy.second + 1<<"\n";
                             return 0;
                         }
                     }
