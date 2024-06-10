7ebf6a3da7dc850a9838d1ae591e622cab5aa78f
Sungbin Shim <everglow83@naver.com>
2024-06-11 00:57:31 +0900

    Title: 세 수의 합, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240611/세 수의 합/sum-of-three-num.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+int main() {
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    unordered_map<int,int> m;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        m[arr[i]]++;
+    }
+    int answer=0;
+    for(int i=0;i<n-2;i++){
+        m[arr[i]]--;
+        for(int j=i+1;j<n-1;j++){
+            if(m[k-arr[i]-arr[j]]){
+                m[arr[j]]--;
+                // cout << arr[i] << " " << arr[j] << " " << k-arr[i]-arr[j] << " " << m[k-arr[i]-arr[j]]<< "\n";
+                answer+=m[k-arr[i]-arr[j]];
+            }
+        }
+    }
+    cout<<answer;
+    return 0;
+}
