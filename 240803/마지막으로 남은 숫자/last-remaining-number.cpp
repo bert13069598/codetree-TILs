773448bec58590861e65512c51f62b3f1a51dbc2
Sungbin Shim <everglow83@naver.com>
2024-08-03 19:48:37 +0900

    Title: 마지막으로 남은 숫자, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240803/마지막으로 남은 숫자/last-remaining-number.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n,x;
+    cin>>n;
+    priority_queue<int> pq;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        pq.push(x);
+    }
+    while(pq.size()>1){
+        int a = pq.top();
+        pq.pop();
+        int b = pq.top();
+        pq.pop();
+        if(a-b)
+            pq.push(a-b);
+    }
+    cout<<pq.top();
+    return 0;
+}
