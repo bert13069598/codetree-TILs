e6884aff0ba50ec95dd58889d4412c7d59bc06b5
Sungbin Shim <everglow83@naver.com>
2024-08-03 22:24:03 +0900

    Title: 앞에서부터 삭제하기 2, Time: 1046ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240803/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
+++ 240803/앞에서부터 삭제하기 2/delete-it-from-the-beginning-2.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <queue>
+#include <deque>
+#include <algorithm>
 using namespace std;
 
 int main() {
     int n,x;
     cin>>n;
-    int N[n];
-    for(int i=0;i<n;i++)
-        cin>>N[i];
+    deque<int> dq;
+    priority_queue<int> pq;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        dq.push_back(x);
+        pq.push(-x);
+        sum+=x;
+    }
     float answer=0;
-    for(int k=1;k<=n-2;k++){
-        priority_queue<int> pq;
-        float sum=0;
-        for(int i=k;i<n;i++){
-            pq.push(-N[i]);
-            sum+=N[i];
-        }
-        sum-=-pq.top();
-        answer=max(answer,sum/(n-k-1));
+    while(dq.size()>2){
+        sum-=dq.front();
+        dq.pop_front();
+        while(find(dq.begin(),dq.end(),-pq.top()) == dq.end())
+            pq.pop();
+        // cout << sum << " " << -pq.top()<<"\n";
+        answer=max(answer, (sum+pq.top())/float(dq.size()-1));
     }
     cout<<fixed;
     cout.precision(2);
