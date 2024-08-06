842f0eed52a191a53a03fb7466dc8e7e038b3401
Sungbin Shim <everglow83@naver.com>
2024-08-06 14:04:22 +0900

    Title: 숫자의 개수, Time: 75ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/숫자의 개수/number-of-integers.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,y;
+    cin>>n>>m;
+    int x[n];
+    for(int i=0;i<n;i++)
+        cin>>x[i];
+    for(int i=0;i<m;i++){
+        cin>>y;
+        int left=0;
+        int right=n-1;
+        int cnt=0;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(x[mid]==y){
+                cnt++;
+                if(x[mid+1]==y)
+                    left=mid+1;
+                else if(x[mid-1]==y)
+                    right=mid-1;
+                else
+                    break;
+            }else if(x[mid]>y)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        cout<<cnt<<"\n";
+    }
+    return 0;
+}
