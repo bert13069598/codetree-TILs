40bf8a1959de93988ffde8817b4ee2f8ddbe377d
Sungbin Shim <everglow83@naver.com>
2024-08-06 14:34:43 +0900

    Title: 가장 먼저 나오는 숫자, Time: 205ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240806/가장 먼저 나오는 숫자/first-appear-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,m,y;
+    cin>>n>>m;
+    int x[n];
+    for(int i=0;i<n;i++)
+        cin>>x[i];
+    for(int i=0;i<m;i++){
+        cin>>y;
+        int left=0;
+        int right=n-1;
+        int idx=-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(x[mid]==y){
+                while(x[mid--]==y)
+                idx=mid+2;
+                break;
+            }else if(x[mid]>y)
+                right=mid-1;
+            else
+                left=mid+1;
+        }
+        cout<<idx<<"\n";
+    }
+    return 0;
+}
