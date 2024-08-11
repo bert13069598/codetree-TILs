5103d076c8e900f369c3d2cc80f3d47830dcb2be
Sungbin Shim <everglow83@naver.com>
2024-08-11 17:14:13 +0900

    Title: 최단 거리 3, Time: 756ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240811/최단 거리 3/shortest-distance-3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <map>
+using namespace std;
+
+int main() {
+    int n,m,s,e,w,a,b;
+    cin>>n>>m;
+    vector<map<int,int>> graph(n+1);
+    for(int i=0;i<m;i++){
+        cin>>s>>e>>w;
+        graph[s][e]=w;
+        graph[e][s]=w;
+    }
+    cin>>a>>b;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>> pq;
+    pq.emplace(0,b);
+    vector<int> visit(n+1,1e8);
+    visit[b]=pq.top().first;
+    while(!pq.empty()){
+        int cost=pq.top().first;
+        int node=pq.top().second;
+        pq.pop();
+        for(auto &[key,value]:graph[node]){
+            int ncost=value+cost;
+            int nnode=key;
+            if(cost<visit[nnode]){
+                visit[nnode]=ncost;
+                pq.emplace(ncost,nnode);
+            }
+        }
+    }
+    cout<<visit[a];
+
+    return 0;
+}
