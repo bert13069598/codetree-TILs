6a4048353c2ab4eff7927b61c2d4c7c54094a107
Sungbin Shim <everglow83@naver.com>
2025-06-22 22:01:44 +0900

    Title: 계단 오르기, Time: 15ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 250622/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+int dfs(int k){
+    if(k==0||k==1)
+        return 1;
+    return k*dfs(k-1);
+}
+
+int main() {
+    cin >> n;
+    int answer=0;
+    for(int x=0;x<=n/2;x++){
+        for(int y=0;y<=n/3;y++){
+            if(2*x+3*y==n){
+                answer+=dfs(x+y)/dfs(x)/dfs(y);
+            }
+        }
+    }  
+    cout<<answer;
+
+    return 0;
+}
