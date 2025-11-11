6198e29ae4911347effb7e580d5a5e9165179070
Sungbin Shim <everglow83@naver.com>
2025-11-11 13:25:03 +0900

    Title: 금 채굴하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251111/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int grid[20][20];
+
+int dx[4]={0,1,-1,0};
+int dy[4]={1,0,0,-1};
+
+int dfs(int k,int x,int y,int grid[20][20],int visit[20][20]){
+    if(k==0)
+        return grid[x][y];
+    int sum=grid[x][y];
+    visit[x][y]=k;
+    for(int i=0;i<4;i++){
+        int nx=x+dx[i];
+        int ny=y+dy[i];
+        if((unsigned)nx>=n||(unsigned)ny>=n)
+            continue;
+        if(visit[nx][ny]<visit[x][y]-1){
+            sum+=dfs(k-1,nx,ny,grid,visit);
+        }
+    }
+    return sum;
+}
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> grid[i][j];
+        }
+    }
+
+    int answer=0;
+    for(int k=0;k<n;k++){
+        int cost=2*k*k+2*k+1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int visit[20][20]{};
+                int sum=dfs(k,i,j,grid,visit);
+                if(k==2){
+                    for(int i=0;i<n;i++){
+                        for(int j=0;j<n;j++)
+                            cout<<visit[i][j]<<" ";
+                        cout<<"\n";
+                    }cout<<sum<<" "<<sum*m<<" "<<cost<<"\n";
+                }
+                if(sum*m-cost>=0)
+                    answer=max(answer,sum);
+            }
+        }
+        
+    }
+
+    cout<<answer;
+
+    return 0;
+}
+
