7f316c42e257ce4e70492c997ea1d20175276602
Sungbin Shim <everglow83@naver.com>
2025-11-19 14:29:14 +0900

    Title: 1차원 젠가, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251119/1차원 젠가/jenga-1d.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int blocks[100];
+int temp[100];
+int s1, e1;
+int s2, e2;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> blocks[i];
+    }
+    cin >> s1 >> e1;
+    cin >> s2 >> e2;
+    s1--;
+    e1--;
+    s2--;
+    e2--;
+
+    int j=0;
+    for(int i=0;i<n;i++){
+        if(i<s1 || i>e1){
+            temp[j]=blocks[i];
+            j++;
+        }
+    }
+    for(int i=0;i<e1-s1+1;i++)
+        blocks[i]=temp[i];
+    
+    j=0;
+    for(int i=0;i<n-(e1-s1+1);i++){
+        if(i<s2 || i>e2){
+            temp[j]=blocks[i];
+            j++;
+        }
+    }
+    for(int i=0;i<n-(e1-s1+1);i++)
+        blocks[i]=temp[i];
+
+    j=n-(e1-s1+1)-(e2-s2+1);
+    cout << j << "\n";
+    for(int i=0;i<j;i++)
+        cout <<blocks[i] << "\n";
+
+    return 0;
+}
