31cd5e8cc64f7395e212174392f8c3addd8bb252
Sungbin Shim <everglow83@naver.com>
2025-11-24 00:54:11 +0900

    Title: 주사위 던지기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251124/주사위 던지기/roll-a-dice.cpp
+++ 251124/주사위 던지기/roll-a-dice.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <map>
 
 using namespace std;
+#define W 0
+#define N 1
+#define B 2
 
 int n, m;
 int r, c;
@@ -11,14 +13,14 @@ char dirs[4]={'L','R','U','D'};
 int grid[100][100]{};
 int dx[4]={0,0,-1,1};
 int dy[4]={-1,1,0,0};
-map<char,int> dice={{'w',4},{'n',5},{'b',6}};
+int dice[3] = {4, 5, 6};  // W:0 N:1 B:2
 
 int main() {
     cin >> n >> m >> r >> c;
     int x=r-1;
     int y=c-1;
 
-    grid[x][y]=dice['b'];
+    grid[x][y]=dice[B];
     for (int i = 0; i < m; i++) {
         cin >> dir;
         int nx,ny;
@@ -28,28 +30,27 @@ int main() {
                 ny=y+dy[j];
                 break;
             }
-
         }
         if((unsigned)nx>=n||(unsigned)ny>=n)
             continue;
         if(dir=='L'){
-            int temp=dice['w'];
-            dice['w']=7-dice['b'];
-            dice['b']=temp;
+            int temp=dice[W];
+            dice[W]=7-dice[B];
+            dice[B]=temp;
         }else if(dir=='R'){
-            int temp=7-dice['w'];
-            dice['w']=dice['b'];
-            dice['b']=temp;
+            int temp=7-dice[W];
+            dice[W]=dice[B];
+            dice[B]=temp;
         }else if(dir=='U'){
-            int temp=dice['n'];
-            dice['n']=7-dice['b'];
-            dice['b']=temp;
+            int temp=dice[N];
+            dice[N]=7-dice[B];
+            dice[B]=temp;
         }else if(dir=='D'){
-            int temp=7-dice['n'];
-            dice['n']=dice['b'];
-            dice['b']=temp;
+            int temp=7-dice[N];
+            dice[N]=dice[B];
+            dice[B]=temp;
         }
-        grid[nx][ny]=dice['b'];
+        grid[nx][ny]=dice[B];
         x=nx;
         y=ny;
     }
