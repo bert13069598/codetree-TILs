b3ac9c2d8e2e61f1a6ab14547cf2fc5ac662d0b9
Sungbin Shim <everglow83@naver.com>
2026-01-26 15:38:37 +0900

    Title: N개 중에 M개 뽑기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260126/N개 중에 M개 뽑기/n-choose-m.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int num[10];
+int cnt[10]{};
+
+void dfs(int now){
+    if(now==N-1){
+        for(int i=0;i<M;i++)
+            cout<<num[i]<<" ";
+        cout<<"\n";
+        return;
+    }
+    for(int i=0;i<N;i++){
+        if(now>0 && num[now-1]>=i+1)
+            continue;            
+        num[now]=i+1;
+        dfs(now+1);
+        num[now]=0;
+    }
+}
+
+int main() {
+    cin >> N >> M;
+
+    dfs(0);
+
+    return 0;
+}
