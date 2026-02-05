75ddb20af13c30de69b5ec81fb22439783b5f95a
Sungbin Shim <everglow83@naver.com>
2026-02-05 23:54:31 +0900

    Title: 최대 점프 횟수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260205/최대 점프 횟수/maximum-number-of-jumps.cpp
+++ 260205/최대 점프 횟수/maximum-number-of-jumps.cpp
@@ -13,18 +13,23 @@ int main() {
     }
 
     a[0]=1;
+    int last=0;
     for(int i=0;i<n-1;i++){
         if(!a[i])
             continue;
+        last=i;
         for(int j=i+1;j<min(n,i+1+arr[i]);j++){
-            if(arr[j])
+            if(arr[j]){
                 a[j]=max(a[j],a[i]+1);
+                last=j;
+            }
         }
     }
     // for(int i=0;i<n;i++)
     //     cout<<a[i]<<" ";
     // cout<<"\n";
 
-    cout<<(a[n-1]==0 ? 1 : a[n-1]-1);
+    // cout<<last<<"\n";
+    cout<<(last==n-1 ? a[last]-1 : a[last]);
     return 0;
 }
