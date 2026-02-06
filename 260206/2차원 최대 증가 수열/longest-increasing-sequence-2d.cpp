8adb06639367b0142a127132209faa5cb3513136
Sungbin Shim <everglow83@naver.com>
2026-02-06 15:24:48 +0900

    Title: 2차원 최대 증가 수열, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260206/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
+++ 260206/2차원 최대 증가 수열/longest-increasing-sequence-2d.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int n, m;
 int grid[50][50];
 int a[50][50];
+bool v[50][50];
 
 int main() {
     cin >> n >> m;
@@ -14,12 +15,17 @@ int main() {
         }
     }
 
+    v[0][0]=true;
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-1;j++){
+            if(!v[i][j])
+                continue;
             for(int p=i+1;p<n;p++){
                 for(int q=j+1;q<n;q++){
-                    if(grid[p][q]>grid[i][j])
+                    if(grid[p][q]>grid[i][j]){
                         a[p][q]=max(a[p][q],a[i][j]+1);
+                        v[p][q]=true;
+                    }
                 }
             }
         }
