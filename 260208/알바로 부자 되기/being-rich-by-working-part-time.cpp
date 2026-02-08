03465af8b933bb5c77a9517d2300b226162f850a
Sungbin Shim <everglow83@naver.com>
2026-02-08 21:32:15 +0900

    Title: 알바로 부자 되기, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260208/알바로 부자 되기/being-rich-by-working-part-time.cpp
+++ 260208/알바로 부자 되기/being-rich-by-working-part-time.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int n;
 int s[1000], e[1000], p[1000];
-int a[1000];
+int a[1000], b[1000];
 
 int main() {
     cin >> n;
@@ -13,16 +13,22 @@ int main() {
         cin >> s[i] >> e[i] >> p[i];
     }
 
-    int answer=0;
     for(int i=0;i<n;i++){
-        int cost=p[i];
+        b[i]=max(b[i],p[i]);
         for(int j=i+1;j<n;j++){
             if((s[i]<e[j])==(e[i]<s[j])){
-                cost+=p[j];
+                a[j]=max(a[j],a[i]+1);
+                b[j]=p[j]+b[i];
             }
         }
-        answer=max(answer,cost);
+        // for(int i=0;i<n;i++)
+        //     cout<<b[i]<<" ";
+        // cout<<"\n";
     }
+
+    int answer=0;
+    for(int i=0;i<n;i++)
+        answer=max(answer,b[i]);
     cout<<answer;
 
     return 0;
