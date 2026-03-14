c4fc1a11964671c19db3f2eb1c6a259ccdb8753e
Sungbin Shim <everglow83@naver.com>
2026-03-14 22:54:29 +0900

    Title: 최소 차 분할, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260314/최소 차 분할/minimum-diff-partition.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+bool dp[100001];
+int sum=0;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum+=arr[i];
+    }
+    int groupa=sum/2;
+    for(int i=0;i<n;i++){
+        for(int j=groupa;j>=0;j--){
+            if(j-arr[i]>=0){
+                dp[j]=true;
+            }
+        }
+    }
+
+
+    int suma=0;
+    for(int i=1;i<=groupa;i++)
+        if(dp[i])
+            suma=max(suma,i);
+    int sumb=sum-suma;
+    cout<<abs(suma-sumb);
+
+    return 0;
+}
