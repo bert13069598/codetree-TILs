c49647edb6903fbcdc8eae59d6d283e19c04999b
Sungbin Shim <everglow83@naver.com>
2026-03-16 17:37:38 +0900

    Title: 계단 오르기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260316/계단 오르기 2/climbing-stairs-2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int a[1001];
+int dp[1001][5];
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    int answer=0;
+    if(n<=4){
+        for(int i=2;i<=n;i++)
+            answer+=a[i];
+        cout<<answer;
+        return 0;
+    }
+    for(int i=1;i<=4;i++){
+        for(int j=i%2;j<n-1;j+=2){
+            // cout<<i<<" "<<j<<"\n";
+            dp[j+2][i]=max(dp[j][i],dp[j+1][i-1])+a[j+2];
+        }
+    }
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=n;j++)
+    //         cout<<dp[j][i]<<" ";
+    //     cout<<"\n";
+    // }
+    cout<<dp[n][4];
+
+    return 0;
+}
