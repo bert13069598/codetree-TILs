539251b72454ebf57a6b42ae036fa87f092cfb00
Sungbin Shim <everglow83@naver.com>
2026-03-22 00:37:56 +0900

    Title: 올바른 등식 만들기, Time: 87ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260322/올바른 등식 만들기/right-equality.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m;
+int nums[100];
+int dp[41][101];
+
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    dp[0][21]=1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<41;j++){
+                dp[i+1][j+nums[i]]+=dp[i][j];
+                dp[i+1][j-nums[i]]+=dp[i][j];
+        }
+    }
+
+    // for(int i=0;i<=n;i++){
+    //     for(int j=0;j<41;j++){
+    //         cout<<dp[i][j]<<" ";
+    //     }
+    //     cout<<"\n";
+    // }
+
+    cout<<dp[n][m+21];
+
+    return 0;
+}
