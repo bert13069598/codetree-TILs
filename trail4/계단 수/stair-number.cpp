d188af5c911d3ce85088f82c888162562b2e2c74
Sungbin Shim <everglow83@naver.com>
2026-05-20 13:54:34 +0900

    [Passed] 계단 수 (2ms, 0MB)

--- trail4/계단 수/stair-number.cpp
+++ trail4/계단 수/stair-number.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int n;
 int dp[1000][10];
+int MOD=1e9+7;
 
 int main() {
     cin >> n;
@@ -13,17 +14,17 @@ int main() {
     for(int i=1;i<n;i++){
         for(int j=0;j<10;j++){
             if(j==0)
-                dp[i][j]=dp[i-1][j+1];
+                dp[i][j]=(dp[i-1][j+1])%MOD;
             else if(j==9)
-                dp[i][j]=dp[i-1][j-1];
+                dp[i][j]=(dp[i-1][j-1])%MOD;
             else
-                dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1];
+                dp[i][j]=(dp[i-1][j-1]+dp[i-1][j+1])%MOD;
         }
     }
 
     int answer=0;
     for(int i=0;i<10;i++)
-        answer+=dp[n-1][i];
+        answer=(answer+dp[n-1][i])%MOD;
     cout<<answer;
 
     return 0;
