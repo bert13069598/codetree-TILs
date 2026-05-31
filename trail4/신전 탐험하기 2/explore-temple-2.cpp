7a27300295b067caee5ae747333fe91268e0def4
Sungbin Shim <everglow83@naver.com>
2026-06-01 00:39:15 +0900

    [Wrong Answer] 신전 탐험하기 2 (2ms, 0MB)

--- trail4/신전 탐험하기 2/explore-temple-2.cpp
+++ trail4/신전 탐험하기 2/explore-temple-2.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int n;
-int l[1001], m[1001], r[1001];
+int lmr[1001][3];
 int dp[1001][3];
 int fi[1001][3];
 
@@ -11,28 +11,33 @@ int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
-        cin >> l[i] >> m[i] >> r[i];
+        cin >> lmr[i][0] >> lmr[i][1] >> lmr[i][2];
     }
 
-    dp[0][0]=l[0];
-    fi[0][0]=0;
-    dp[0][1]=m[0];
-    fi[0][1]=1;
-    dp[0][2]=r[0];
-    fi[0][2]=2;
+    for(int i=0;i<3;i++){
+        dp[0][i]=lmr[0][i];
+        fi[0][i]=i;
+    }
 
     for(int i=1;i<n;i++){
-        dp[i][0]=l[i]+max(m[i-1],r[i-1]);
-        fi[i][0]=m[i-1]>r[i-1] ? fi[i-1][1] : fi[i-1][2]; 
-        dp[i][1]=m[i]+max(l[i-1],r[i-1]);
-        fi[i][1]=l[i-1]>r[i-1] ? fi[i-1][0] : fi[i-1][2]; 
-        dp[i][2]=r[i]+max(l[i-1],m[i-1]);
-        fi[i][2]=l[i-1]>m[i-1] ? fi[i-1][1] : fi[i-1][1]; 
+        for(int j=0;j<3;j++){
+            int prev=0;
+            for(int k=0;k<3;k++){
+                if(j==k)
+                    continue;
+                if(i==n-1 && j==fi[i-1][k])
+                    continue;
+                if(dp[i-1][k]>prev){
+                    prev=dp[i-1][k];
+                    fi[i][j]=fi[i-1][k];
+                }
+            }
+            dp[i][j]=lmr[i][j]+prev;
+        }
     }
+
     int answer=0;
     for(int i=0;i<3;i++){
-        if(fi[n-1][i]==fi[0][i])
-            dp[n-1][i]=0;
         answer=max(answer,dp[n-1][i]);
     }
     cout<<answer;
